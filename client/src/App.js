4c58cb917585b4a0dadfb3320851d0f62f6d648c
Amorem <dev@amorem.com>
2021-05-03 12:31:34 +0200

    using use-shopping-cart

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,29 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 import Result from "./pages/Result";
+import { loadStripe } from "@stripe/stripe-js";
+import { CartProvider } from "use-shopping-cart";
 
 const queryClient = new QueryClient();
+
+const StripePromise = loadStripe("pk_test_6FTOvxXVJH9h3yyPI7ZT4Kki");
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/result" component={Result} />
-          <Route path="/:productId" component={Product} />
-        </Switch>
-      </BrowserRouter>
+      <CartProvider
+        mode="checkout-session"
+        stripe={StripePromise}
+        currency="USD"
+      >
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/result" component={Result} />
+            <Route path="/:productId" component={Product} />
+          </Switch>
+        </BrowserRouter>
+      </CartProvider>
     </QueryClientProvider>
   );
 }
