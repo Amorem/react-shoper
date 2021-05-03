467d9a9eca4ca62e9466beff94ebeae375790c76
Amorem <dev@amorem.com>
2021-05-03 16:25:49 +0200

    checkout with Stripe

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_API_SECRET);
 const express = require("express");
 const products = require("./products.json");
+const { validateCartItems } = require("use-shopping-cart/src/serverUtil");
 
 module.exports = function getRoutes() {
   const router = express.Router();
@@ -8,6 +10,8 @@ module.exports = function getRoutes() {
   router.get("/products", getProducts);
 
   router.get("/products/:productId", getProduct);
+
+  router.post("/checkout-sessions", createCheckoutSessions);
 
   return router;
 };
@@ -27,4 +31,32 @@ function getProduct(req, res) {
     res.status(404).json({ statusCode: 404, message: error.message });
   }
   res.status(200).json({ product });
+}
+
+const origin =
+  process.env.NODE_ENV === "production"
+    ? req.headers.origin
+    : "http://localhost:3000";
+
+async function createCheckoutSessions(req, res) {
+  try {
+    const cartItems = req.body;
+    const line_items = validateCartItems(products, cartItems);
+    const params = {
+      submit_type: "pay",
+      payment_method_types: ["card"],
+      billing_address_collection: "auto",
+      shipping_address_collection: {
+        allowed_countries: ["US"],
+      },
+      line_items,
+      success_url: `${origin}/result?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: origin,
+      mode: "payment",
+    };
+    const checkoutSession = await stripe.checkout.sessions.create(params);
+    res.status(200).json(checkoutSession);
+  } catch (error) {
+    res.status(500).json({ statusCode: 500, message: error.message });
+  }
 }
